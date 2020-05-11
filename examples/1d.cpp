5dd41f086b6e5cc6648fc8dfee8aba296cb39f91
wenyan4work <wenyan4work@gmail.com>
2020-05-11 13:59:59 -0400

    test accuracy

--- examples/1d.cpp
+++ examples/1d.cpp
@@ -15,44 +15,44 @@
 using namespace mlinterp;
 using namespace std;
 
+double func(double x) { return sin(x); }
+
 int main() {
 
-	// Boundaries of the interval [-pi, pi]
-	constexpr double b = 3.14159265358979323846, a = -b;
+    // Boundaries of the interval [-pi, pi]
+    constexpr double b = 3.14159265358979323846, a = -b;
 
-	// Subdivide the interval [-pi, pi] using 15 evenly-spaced points and
-	// evaluate sin(x) at each of those points
-	constexpr int nxd = 15, nd[] = { nxd };
-	double xd[nxd];
-	double yd[nxd];
-	for(int n = 0; n < nxd; ++n) {
-		xd[n] = a + (b - a) / (nxd - 1) * n;
-		yd[n] = sin(xd[n]);
-	}
+    // Subdivide the interval [-pi, pi] using 15 evenly-spaced points and
+    // evaluate sin(x) at each of those points
+    constexpr int nxd = 15, nd[] = {nxd};
+    double xd[nxd];
+    double yd[nxd];
+    for (int n = 0; n < nxd; ++n) {
+        xd[n] = a + (b - a) / (nxd - 1) * n;
+        yd[n] = func(xd[n]);
+    }
 
-	// Subdivide the interval [-pi, pi] using 100 evenly-spaced points
-	// (these are the points at which we interpolate)
-	constexpr int ni = 100;
-	double xi[ni];
-	for(int n = 0; n < ni; ++n) {
-		xi[n] = a + (b - a) / (ni - 1) * n;
-	}
+    // Subdivide the interval [-pi, pi] using 100 evenly-spaced points
+    // (these are the points at which we interpolate)
+    constexpr int ni = 100;
+    double xi[ni];
+    for (int n = 0; n < ni; ++n) {
+        xi[n] = a + (b - a) / (ni - 1) * n;
+    }
 
-	// Perform the interpolation
-	double yi[ni]; // Result is stored in this buffer
-	interp(
-		nd, ni, // Number of points
-		yd, yi, // Output axis (y)
-		xd, xi  // Input axis (x)
-	);
+    // Perform the interpolation
+    double yi[ni]; // Result is stored in this buffer
+    interp(nd, ni, // Number of points
+           yd, yi, // Output axis (y)
+           xd, xi  // Input axis (x)
+    );
 
-	// Print the interpolated values
-	cout << scientific << setprecision(8) << showpos;
-	for(int n = 0; n < ni; ++n) {
-		cout << xi[n] << "\t" << yi[n] << endl;
-	}
+    // Print the interpolated values
+    cout << scientific << setprecision(8) << showpos;
+    for (int n = 0; n < ni; ++n) {
+        const double val = func(xi[n]);
+        cout << xi[n] << "\t" << yi[n] << "\t" << (yi[n] - val) / val << endl;
+    }
 
-	return 0;
-
+    return 0;
 }
-
--- examples/2d.cpp
+++ examples/2d.cpp
@@ -16,60 +16,60 @@
 using namespace mlinterp;
 using namespace std;
 
+double func(double x, double y) { return sin(x) * cos(y); }
+
 int main() {
 
-	// Boundaries of the interval [-pi, pi]
-	constexpr double b = 3.14159265358979323846, a = -b;
+    // Boundaries of the interval [-pi, pi]
+    constexpr double b = 3.14159265358979323846, a = -b;
 
-	// Discretize the set [-pi, pi] X [-pi, pi] using 15 evenly-spaced
-	// points along the x axis and 15 evenly-spaced points along the y axis
-	// and evaluate sin(x)cos(y) at each of those points
-	constexpr int nxd = 15, nyd = 15, nd[] = { nxd, nyd };
-	double xd[nxd];
-	for(int i = 0; i < nxd; ++i) {
-		xd[i] = a + (b - a) / (nxd - 1) * i;
-	}
-	double yd[nyd];
-	for(int j = 0; j < nyd; ++j) {
-		yd[j] = a + (b - a) / (nyd - 1) * j;
-	}
-	double zd[nxd * nyd];
-	for(int i = 0; i < nxd; ++i) {
-		for(int j = 0; j < nyd; ++j) {
-			const int n = j + i * nyd;
-			zd[n] = sin(xd[i]) * cos(yd[j]);
-		}
-	}
+    // Discretize the set [-pi, pi] X [-pi, pi] using 15 evenly-spaced
+    // points along the x axis and 15 evenly-spaced points along the y axis
+    // and evaluate sin(x)cos(y) at each of those points
+    constexpr int nxd = 15, nyd = 15, nd[] = {nxd, nyd};
+    double xd[nxd];
+    for (int i = 0; i < nxd; ++i) {
+        xd[i] = a + (b - a) / (nxd - 1) * i;
+    }
+    double yd[nyd];
+    for (int j = 0; j < nyd; ++j) {
+        yd[j] = a + (b - a) / (nyd - 1) * j;
+    }
+    double zd[nxd * nyd];
+    for (int i = 0; i < nxd; ++i) {
+        for (int j = 0; j < nyd; ++j) {
+            const int n = j + i * nyd;
+            zd[n] = func(xd[i], yd[j]);
+        }
+    }
 
-	// Subdivide the set [-pi, pi] X [-pi, pi] using 100 evenly-spaced
-	// points along the x axis and 100 evenly-spaced points along the y axis
-	// (these are the points at which we interpolate)
-	constexpr int m = 100, ni = m * m;
-	double xi[ni];
-	double yi[ni];
-	for(int i = 0; i < m; ++i) {
-		for(int j = 0; j < m; ++j) {
-			const int n = j + i * m;
-			xi[n] = a + (b - a) / (m - 1) * i;
-			yi[n] = a + (b - a) / (m - 1) * j;
-		}
-	}
+    // Subdivide the set [-pi, pi] X [-pi, pi] using 100 evenly-spaced
+    // points along the x axis and 100 evenly-spaced points along the y axis
+    // (these are the points at which we interpolate)
+    constexpr int m = 100, ni = m * m;
+    double xi[ni];
+    double yi[ni];
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < m; ++j) {
+            const int n = j + i * m;
+            xi[n] = a + (b - a) / (m - 1) * i;
+            yi[n] = a + (b - a) / (m - 1) * j;
+        }
+    }
 
-	// Perform the interpolation
-	double zi[ni]; // Result is stored in this buffer
-	interp(
-		nd, ni,        // Number of points
-		zd, zi,        // Output axis (z)
-		xd, xi, yd, yi // Input axes (x and y)
-	);
+    // Perform the interpolation
+    double zi[ni];        // Result is stored in this buffer
+    interp(nd, ni,        // Number of points
+           zd, zi,        // Output axis (z)
+           xd, xi, yd, yi // Input axes (x and y)
+    );
 
-	// Print the interpolated values
-	cout << scientific << setprecision(8) << showpos;
-	for(int n = 0; n < ni; ++n) {
-		cout << xi[n] << "\t" << yi[n] << "\t" << zi[n] << endl;
-	}
+    // Print the interpolated values
+    cout << scientific << setprecision(8) << showpos;
+    for (int n = 0; n < ni; ++n) {
+        const double val = func(xi[n], yi[n]);
+        cout << xi[n] << "\t" << yi[n] << "\t" << zi[n] << "\t" << (zi[n] - val) / val << endl;
+    }
 
-	return 0;
-
+    return 0;
 }
-
